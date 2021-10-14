df998b72c07f60d06c69d071910b01ba9113c566
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-10-14 15:14:13 +0500

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./styling/app.css";
 import Homepage from './Components/Homepage';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Homepage />
     </div>
   );
